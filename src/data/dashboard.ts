ec0277267d20f2d6a6304bbfc2a7c19918070783
Dmytro Osaula <dimaosaula93@gmail.com>
2026-09-21 16:07:52 +0300

    Add dynamic dashboard statistics
    Add dynamic dashboard statistics

--- src/data/dashboard.ts
+++ src/data/dashboard.ts
@@ -1,30 +1,32 @@
-import { ListTodo, Clock3, CircleCheckBig, type LucideIcon } from "lucide-react";
+import {
+  ListTodo,
+  Clock3,
+  CircleCheckBig,
+  type LucideIcon,
+} from "lucide-react";
+
 import type { DashboardInfo } from "../utils/taskStyles";
 
-type dashInfo = {
-    id: number;
-    icon: LucideIcon;
-    description: DashboardInfo;
-    quantity: number;
-}
+type DashInfo = {
+  id: number;
+  icon: LucideIcon;
+  description: DashboardInfo;
+};
 
-export const dashboardInfo: dashInfo[] = [
-    {
-        id: 1,
-        icon: ListTodo,
-        description: "Total Tasks",
-        quantity: 12
-    },
-    {
-        id: 2,
-        icon: Clock3,
-        description: "In Progress",
-        quantity: 4
-    },
-    {
-        id: 3,
-        icon: CircleCheckBig,
-        description: "Completed",
-        quantity: 8
-    },
-]
+export const dashboardInfo: DashInfo[] = [
+  {
+    id: 1,
+    icon: ListTodo,
+    description: "Total Tasks",
+  },
+  {
+    id: 2,
+    icon: Clock3,
+    description: "In Progress",
+  },
+  {
+    id: 3,
+    icon: CircleCheckBig,
+    description: "Completed",
+  },
+];
