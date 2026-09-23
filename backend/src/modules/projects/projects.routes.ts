678707d75de264e38e38e3fcd739a49f3a957466
Dmytro Osaula <dimaosaula93@gmail.com>
2026-09-23 17:02:41 +0300

    update routes

--- backend/src/modules/projects/projects.routes.ts
+++ backend/src/modules/projects/projects.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getProjects, getProject } from "./projects.controller.js";
+import { getProjects, getProject, postProject } from "./projects.controller.js";
 
 const router = Router();
 
 router.get("/", getProjects);
 router.get("/:id", getProject );
+router.post("/", postProject)
 
 export default router;
